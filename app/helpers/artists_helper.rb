a7829da2803285633496513266cda45aa40bc037
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-06-20 14:02:38 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        if song.artist && song.artist.name
+            link_to song.artist.name, artist_path(song.artist), method: :get
+        else
+            link_to "Add Artist", edit_song_path(song), method: :get
+        end
+    end
 end
